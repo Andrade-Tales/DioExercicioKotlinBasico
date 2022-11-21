2ce03a21c989326c5912e44e9e8fea30f1c64f54
Tales Andrade <selatedardna.1476@gmail.com>
2022-11-21 19:30:34 -0300

    Exercícios matemáticos básicos

---
+++ app/src/main/java/com/bootcamp/dioexerciciokotlinbasico/exercicios/ConsumoMedioAutomovel.kt
@@ -0,0 +1,38 @@
+package com.bootcamp.dioexerciciokotlinbasico.exercicios
+
+import java.util.*
+
+class ConsumoMedioAutomovel {
+
+    // Consumo Médio do Automóvel
+
+/*
+Você deve calcular o consumo médio de um automóvel onde será informada a
+distância total percorrida (em Km) e o total de combustível consumido (em
+litros).
+- Entrada
+Você receberá dois valores: um valor inteiro X com a distância total percorrida
+(em Km), e um valor real Y que representa o total de combustível consumido,
+com um dígito após o ponto decimal.
+- Saída
+Exiba o valor que representa o consumo médio do automóvel (3 casas após a
+vírgula), incluindo no final a mensagem "km/l".
+*/
+
+}
+
+fun main(args: Array<String>) {
+
+    val leitor = Scanner(System.`in`)
+
+    println("Entre com a distância total percorrida em km: ")
+    val x: Int = leitor.nextInt()
+
+    println("Entre com o total de combustível consimido: ")
+    val y = leitor.nextFloat()
+
+    println("A média de consumo é: ")
+    val media = x.div(y)
+
+    println(String.format("%.3f km/l", media))
+}
---
+++ app/src/main/java/com/bootcamp/dioexerciciokotlinbasico/exercicios/ConversaoDeTempo.kt
@@ -0,0 +1,34 @@
+package com.bootcamp.dioexerciciokotlinbasico.exercicios
+
+class ConversaoDeTempo {
+
+    // Conversão de Tempo
+
+/*
+Você terá o desafio de ler um valor inteiro, que é o tempo de duração em
+segundos de um determinado evento em uma loja, e informe-o expresso no formato
+horas:minutos:segundos.
+•	Entrada
+O arquivo de entrada contém um valor inteiro N.
+•	Saída
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para
+horas:minutos:segundos, conforme exemplo fornecido.
+*/
+
+}
+
+fun main(args: Array<String>) {
+
+    println("Entre com um valor inteiro: ")
+    val time = readLine()!!.toInt()
+
+    val hour = (time / 3600)
+
+    val minutes = (time % 3600) / 60
+
+    val seconds = (time % 3600) % 60
+
+
+    println("$hour:$minutes:$seconds")
+}
+
